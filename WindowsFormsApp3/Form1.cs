a42e64ba5a52f5b8283a386a988d48f50ac4e175
unknown <iolanta.paly@gmail.com>
2018-10-07 23:40:20 +0300

    fixed bugs

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -156,32 +156,37 @@ namespace WindowsFormsApp3
 
         void point_in_polygon(PointF n)
         {
-            if (point_list.Contains(n))
-                g.DrawEllipse(new Pen(Color.Chocolate), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
-            else
-            {
-                int cnt_intersec = 0;
-                PointF intersec;
-                PointF finish = new PointF(0, 0);
-                for(var i = 0; i < point_list.Count - 1; ++i)
+            for (int i = 0; i < transofmed_points.GetLength(0); ++i) 
+                if (transofmed_points[i, 0] == n.X && transofmed_points[i, 1] == n.Y)
                 {
-                    if (FindIntersection(n, finish, point_list[i], point_list[i + 1], out intersec) && intersec != point_list[i])
-                        ++cnt_intersec;
+                    g.DrawEllipse(new Pen(Color.Chocolate), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
+                    return;
                 }
 
-                if (FindIntersection(n, finish, point_list[point_list.Count - 1], point_list[0], out intersec) && intersec != point_list[point_list.Count - 1])
-                    ++cnt_intersec;
-
-                if (cnt_intersec % 2 != 0)     // принадлежит многоугольнику
-                    g.DrawEllipse(new Pen(Color.Blue), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
-                else                           // не принадлежит
-                    g.DrawEllipse(new Pen(Color.Green), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
+            int cnt_intersec = 0;
+            PointF intersec;
+            PointF finish = new PointF(0, 0);
+            for(var i = 0; i < transofmed_points.GetLength(0)-1; ++i)
+            {
+                if (FindIntersection(n, finish, new PointF(transofmed_points[i, 0], transofmed_points[i, 1]), new PointF(transofmed_points[i + 1, 0], transofmed_points[i + 1, 1]), out intersec) 
+                    && intersec != new PointF(transofmed_points[i, 0], transofmed_points[i, 1]))
+                ++cnt_intersec;
             }
+
+            PointF p1 = new PointF(transofmed_points[transofmed_points.GetLength(0) - 1, 0], transofmed_points[transofmed_points.GetLength(0) - 1, 1]);
+            PointF p2 = new PointF(transofmed_points[0, 0], transofmed_points[0, 1]);
+            if (FindIntersection(n, finish, p1, p2, out intersec) && intersec != p1)
+                ++cnt_intersec;
+
+            if (cnt_intersec % 2 != 0)     // принадлежит многоугольнику
+                g.DrawEllipse(new Pen(Color.Blue), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
+            else                           // не принадлежит
+                g.DrawEllipse(new Pen(Color.Green), new Rectangle((int)n.X - 1, (int)n.Y - 1, 3, 3));
         }
 
         private void point_of_intersection()
         {
-            if (FindIntersection(two_points[0], two_points[1], point_list[0], point_list[1], out PointF intersec))
+            if (FindIntersection(two_points[0], two_points[1], new PointF(transofmed_points[0, 0], transofmed_points[0, 1]), new PointF(transofmed_points[1, 0], transofmed_points[1, 1]), out PointF intersec))
                 g.DrawEllipse(new Pen(Color.Fuchsia, 4), new Rectangle((int)intersec.X - 1, (int)intersec.Y - 1, 4, 4));
             two_points.Clear();
         }
